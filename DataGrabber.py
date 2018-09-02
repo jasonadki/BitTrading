26d2313c54977104d9c39ccbf7572fc23ac9df8b
jasonadki <jasonadkin@gmail.com>
2018-09-02 17:08:23 -0400

    Abstracted DataGrabber class instead of inside Grapher

---
+++ DataGrabber.py
@@ -0,0 +1,66 @@
+from Bittrex import Bittrex
+import pandas as pd
+import json
+from datetime import datetime
+import matplotlib.pyplot as plt
+import csv
+
+
+
+
+
+
+class DataGrabber():
+	"""
+	Used for historical data. Making a unqiue class to hold these because
+	it will be used in a graphing and testing applications
+	"""
+
+	def __init__(self, secrets, market, timeInterval, priceType):
+		self.Bittrex = Bittrex(secrets)
+		self.market = market
+		self.timeInterval = timeInterval
+		self.priceType = priceType
+		self.data = pd.DataFrame()
+
+
+	def clean_historical_data(self, brokenDate):
+		"""
+		The Bittrex historical data gives time values as '2018-08-27T23:51:00'.
+		This function will clean this into a datetime variable.
+
+		:param brokenDate: <str> The messy date from Bittrex
+		:return: <datetime> New clean datetime value
+		"""
+
+		return datetime.strptime(brokenDate, '%Y-%m-%dT%H:%M:%S')
+
+
+
+	def get_data(self):
+		"""
+		Grab the historical data from Bittrex for the given market
+		and return as a Pandas DataFrame.
+		(Because who doesn't like Pandas??)
+		
+		:return: <DataFrame> Two column dataframe of time and price.
+		"""
+
+		# Create naming dictionary
+		nameDic = {'O':'Open', 'H':'High', 'L':'Low', 'C':'Close', 'V':'Volume', 'T':'Time', 'BV':'BookValue'}
+
+		# Get Bittrex historical data
+		data = self.Bittrex.get_ticks(self.market, self.timeInterval)['result']
+
+		# Create dataframe only take the time and chosen variable
+		df = pd.DataFrame(data)[['T', self.priceType]]
+
+		# Map column names to appropraite column values
+		df.columns = list(map(nameDic.get, list(df)))
+
+		# Fix the time variable that Bittrex gives
+		df['Time'] = df['Time'].apply(self.clean_historical_data)
+
+		self.data = df
+
+		return df
--- Tester.py
+++ Tester.py
@@ -1,4 +1,5 @@
 from Bittrex import Bittrex
+from DataGrabber import DataGrabber
 import pandas as pd
 import json
 
@@ -6,42 +7,16 @@ import json
 
 
 
-class Grapher():
-	"""
-	Used for handeling all testing functionality
-	"""
 
-	def __init__(self, secrets, market, timeInterval):
-		self.Bittrex = Bittrex(secrets)
-		self.market = market
-		self.timeInterval = timeInterval
+if __name__ == '__main__':
+	
+	with open('./database/secrets.json') as file:
+		secrets = json.load(file)
+		file.close()
 
-	def getData(self):
-		data = self.Bittrex.get_ticks(self.market, self.timeInterval)
+	data = DataGrabber(secrets, 'BTC-RVN', 'day', 'O').get_data()
 
-		return data
+	data = data.drop(data.index[0])
 
-
-
-with open('./database/secrets.json') as file:
-	secrets = json.load(file)
-	file.close()
-
-test = Tester(secrets, 'BTC-RVN', 'day')
-
-print(test.getData())
-
-# bit = Bittrex('5049a21e38da48389cc641ffa97cf10b', 'c0eb970db3ec48a5b9d99dff6fced2b1')
-
-
-# df = bit.get_ticks('BTC-RVN', 'day')['result']
-
-
-# dic = df[0]
-
-# print((dic['T'], dic['O']))
-# vals = [(d['T'], d['O']) for d in df]
-
-# print(vals)
-
-# # 2018-08-27T23:51:00
+	
+	
